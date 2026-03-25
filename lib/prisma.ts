bd272a983d560165a1059435348724ccb13dc87f
Diego Colombres <dcolombres.produccion@gmail.com>
2026-03-25 12:37:01 -0300

    fix: Explicitly type global.prisma as per user's suggestion

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,12 @@
 import { PrismaClient } from '@prisma/client';
 
+// add prisma to the NodeJS global type
+// This declaration is needed for TypeScript to recognize `global.prisma`
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
+
 // This is a common pattern to prevent multiple instances of Prisma Client in development
 // by attaching it to the global object.
 const prisma = global.prisma || new PrismaClient();
